e7f3547beb251bfe109c0097c1b8b8cec7069354
mtonholli <matheustonholli@hotmail.com>
2023-10-20 05:49:47 -0300

    entrega do desafio Virtual Piano. errata

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
-const keysCheck = document.querySelector(".keys-check input")
+const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
@@ -16,25 +16,25 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click",()=> playTune(key.dataset.key));
-    mapedKeys.push("key.dataset.key")
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
     if(mapedKeys.includes(e.key)){
     playTune(e.key);
-}})
+}});
 
 const handleVolume = (e) => {
     audio.volume = e.target.value;
 };
 
 const showHideKeys = () => {
-    pianoKeys.forEach(key => key.classList.toggle("hide"))
-}
+    pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
 
 volumeSlider.addEventListener("input", handleVolume);
 
-keysCheck.addEventListener("click", showHideKeys)
+keysCheck.addEventListener("click", showHideKeys);
 
 
 
